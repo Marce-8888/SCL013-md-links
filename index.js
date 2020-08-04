2511e53d731ebd935e63fea68ac57d3876a18f77
marce-8888 <mtancarao@outlook.cl>
2020-08-04 14:05:52 -0400

    funciones para validar en 200 y 404 e instalacion de librerías

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const path = require ("path");//Contiene métodos para trabajar con rutas
-const fs = require ("fs");//Nos permite acceder al sistema de archivos, viene incorporado en node
+const path = require ('path');//Contiene métodos para trabajar con rutas
+const fs = require ('fs');//Nos permite acceder al sistema de archivos, viene incorporado en node
 let file = process.argv[2];//Toma lo que se le da desde consola
-const axios = require ('axios');//librería para hacer solicitudes HTTP
+//const axios = require ('axios');//librería para hacer solicitudes HTTP
+const fetch = require ('fetch');//librería para hacer solicitudes HTTP
+const fetchUrl = fetch.fetchUrl;
+const jsdom = require('jsdom');//Transforma el DOM en html
+const { JSDOM } = jsdom;
+const showdown = require('showdown');//Transforma el MD en html
+const colors = require('colors');//Librería para poner colores a la respuestas en consola
+
+/* const options = (file) => {
+  console.log("veamos", file)
+  let validate = false;
+  let stats = false;
+validate = file.includes("--validate");
+stats = file.includes("--stats");
+console.log('probando', validate)
+} */
 
 //Función que lee el archivo
 const getMd = (file) => {
@@ -12,6 +27,7 @@ const getMd = (file) => {
       } else {
         resolve(file)
       }
+
     })
   })
 }
@@ -32,60 +48,38 @@ if (path.extname(file) === '.md') {
 //Función que lee los links de la ruta espeficificada
 const expReg = /(((https?:\/\/)|(http?:\/\/)|(www\.))[^\s\n]+)(?=\))/g;
 
-fs.readFile(file, "utf-8", (err,file) => {
+let arrLinks = [];//hice una variable para poder retornarla
+
+fs.readFile(file, 'utf-8', (err,file) => {
   if (err){
     console.log(err);
   }else{
-    console.log(file.match(expReg));
-  }
+     let links = file.match(expReg);
+      console.log(file.match(expReg));
+        links.forEach(element => {
+          getStatus(element)
+           .then (res =>{
+             console.log('El link', element, 'es', res);
+            })
+           .catch (err => {
+             console.log(err);
+           })
+        })
+   }
+   return arrLinks;//que nos retorne la variable para poder llamarla en la siguiente función
 });
 
 
-/*const validateHref = (links) => {
-  return new Promise((resolve, reject) => {
-    let validated = [];
-    links.forEach(link => {
-      validated.push(
-        fetch(link.href)
-        .then(response => {
-          return {
-            ...link,
-            status: response.status
-          }
-        })
-        .catch((error) => {
-            return {
-              ...link,
-              status: `Fail 404, ${error.message}`
-            }
-        })
-      )
+//Función que obtiene la URL validandola en 200 y 404
+const getStatus = (url) => {
+  return new Promise ((resolve, reject) => {
+    fetchUrl(url, (error, meta) => {
+      if (error) {
+        reject (error)
+      } else {
+        resolve (meta.status);
+      }
     })
-    resolve(Promise.all(validated)); //equivale a los resultados de todas las promesas
-  })
-}*/
-
-//Validar Links
-/*const validate = (links) => {
-  const arrValidate = [];
-  const arrLinks = expReg(links);
-    arrLinks.forEach((element) => {
-      arrValidate.push(axios.get(element.href)
-      .then((res) => {
-        const newObj = {
-          ...element,
-          status: res.status,
-          statusText: res.statusText,
-        };
-        return newObj;
-      })
-      .catch(() => ({
-        ...element,
-        status: 400,
-        statusText: 'FAIL',
-      }))
-      );
-    });
-    return Promise.all(arrValidate);
+  });
 };
-*/
+
